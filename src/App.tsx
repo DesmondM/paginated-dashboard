9e96a03a26e37c42affcbf0af5482767329bea27
Desmond <desmondmpofu@gmail.com>
2022-09-18 18:00:11 +0200

    additions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,4 @@
-import {
-  JSXElementConstructor,
-  Key,
-  ReactElement,
-  ReactFragment,
-  ReactPortal,
-  useEffect,
-  useState,
-} from "react";
+import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -65,13 +57,34 @@ function App() {
           </svg>
           <h4>Smartwage</h4>
         </div>
-        <div className="items">
+        {/* <div className="items">
           <li>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="icon icon-tabler icon-tabler-letter-m"
-              width="44"
-              height="44"
+              width="22"
+              height="22"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#2c3e50"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M6 20v-16l6 14l6 -14v16" />
+            </svg>{" "}
+            <h4>The M</h4>
+          </li>
+        </div> */}
+        <div className="at-foot">
+          <div className="foot-col">
+            {" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-letter-m"
+              width="22"
+              height="22"
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="#2c3e50"
@@ -82,14 +95,35 @@ function App() {
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M6 20v-16l6 14l6 -14v16" />
             </svg>
-          </li>
+          </div>
+          <div className="foot-col">Mike Mekenekep</div>
+          <div className="foot-col">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-arrows-down-up"
+              width="22"
+              height="22"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#2c3e50"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <line x1="17" y1="3" x2="17" y2="21" />
+              <path d="M10 18l-3 3l-3 -3" />
+              <line x1="7" y1="21" x2="7" y2="3" />
+              <path d="M20 6l-3 -3l-3 3" />
+            </svg>
+          </div>
         </div>
       </section>
 
       <section id="interface">
         <div className="navigation">
           <div className="n1">
-            <div className="search">Surveys</div>
+            <div className="heading">Surveys</div>
           </div>
         </div>
         <div className="board">
@@ -115,22 +149,57 @@ function App() {
                     <td>{survey.close_date}</td>
                     <td>
                       {survey.status === "Live" ? (
-                        <p className="status">{survey.status}</p>
+                        <p className="live">{survey.status}</p>
                       ) : (
-                        <p style={{ color: "red" }}>{survey.status}</p>
+                        <p className="closed">{survey.status}</p>
                       )}
                     </td>
                   </tr>
                 );
               })}
             </tbody>
-            <div>
-              Rows per page 10 | {pageCount} of{" "}
-              {Math.ceil(surveyData.length / 10)}{" "}
-              <span onClick={backCount}>{"< "}</span>
-              <span onClick={forwardCount}>{" >"}</span>
-            </div>
           </table>
+        </div>
+        <hr
+          style={{ width: "90%", textAlign: "left", marginLeft: "30px" }}
+        ></hr>
+        <div className="pagination">
+          Rows per page 10 | {pageCount} of {Math.ceil(surveyData.length / 10)}{" "}
+          <span onClick={backCount}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-caret-left"
+              width="22"
+              height="22"
+              viewBox="0 0 24 12"
+              stroke-width="1"
+              stroke="#2c3e50"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M18 15l-6 -6l-6 6h12" transform="rotate(270 12 12)" />
+            </svg>
+          </span>
+          <span onClick={forwardCount}>
+            {" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-caret-right"
+              width="22"
+              height="22"
+              viewBox="0 0 24 12"
+              stroke-width="1"
+              stroke="#2c3e50"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M18 15l-6 -6l-6 6h12" transform="rotate(90 12 12)" />
+            </svg>
+          </span>
         </div>
       </section>
 
