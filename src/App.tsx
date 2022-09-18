51a1ebc3e8d8ad53d25164df77fbe6eec2046e3d
Desmond <desmondmpofu@gmail.com>
2022-09-18 09:58:01 +0200

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -43,15 +43,35 @@ function App() {
   };
 
   return (
-    <>
-      <div className="App">
-        <section id="menu">
-          <div className="logo">
+    <div className="App">
+      <section id="menu">
+        <div className="logo">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-menu-2"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <line x1="4" y1="6" x2="20" y2="6" />
+            <line x1="4" y1="12" x2="20" y2="12" />
+            <line x1="4" y1="18" x2="20" y2="18" />
+          </svg>
+          <h4>Smartwage</h4>
+        </div>
+        <div className="items">
+          <li>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-menu-2"
-              width="20"
-              height="20"
+              className="icon icon-tabler icon-tabler-letter-m"
+              width="44"
+              height="44"
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="#2c3e50"
@@ -60,83 +80,62 @@ function App() {
               stroke-linejoin="round"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <line x1="4" y1="6" x2="20" y2="6" />
-              <line x1="4" y1="12" x2="20" y2="12" />
-              <line x1="4" y1="18" x2="20" y2="18" />
+              <path d="M6 20v-16l6 14l6 -14v16" />
             </svg>
-            <h4>Smartwage</h4>
-          </div>
-          <div className="items">
-            <li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="icon icon-tabler icon-tabler-letter-m"
-                width="44"
-                height="44"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="#2c3e50"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M6 20v-16l6 14l6 -14v16" />
-              </svg>
-            </li>
-          </div>
-        </section>
+          </li>
+        </div>
+      </section>
 
-        <section id="interface">
-          <div className="navigation">
-            <div className="n1">
-              <div className="search">Surveys</div>
+      <section id="interface">
+        <div className="navigation">
+          <div className="n1">
+            <div className="search">Surveys</div>
+          </div>
+        </div>
+        <div className="board">
+          <table style={{ width: "100%", borderSpacing: "0px" }}>
+            <thead>
+              <tr>
+                <td>Survey Name</td>
+                <td>Created By</td>
+                <td>Responses</td>
+                <td>Launch Date</td>
+                <td>Close Date</td>
+                <td>Status</td>
+              </tr>
+            </thead>
+            <tbody>
+              {surveyData.slice(startAt, endAt).map((survey) => {
+                return (
+                  <tr key={survey.id}>
+                    <td>{survey.name}</td>
+                    <td>{survey.created_by}</td>
+                    <td>{survey.response}</td>
+                    <td>{survey.launch_date}</td>
+                    <td>{survey.close_date}</td>
+                    <td>
+                      {survey.status === "Live" ? (
+                        <p className="status">{survey.status}</p>
+                      ) : (
+                        <p style={{ color: "red" }}>{survey.status}</p>
+                      )}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+            <div>
+              Rows per page 10 | {pageCount} of{" "}
+              {Math.ceil(surveyData.length / 10)}{" "}
+              <span onClick={backCount}>{"< "}</span>
+              <span onClick={forwardCount}>{" >"}</span>
             </div>
-          </div>
-          <div className="board">
-            <table style={{ width: "100%" }}>
-              <thead>
-                <tr>
-                  <td>Survey Name</td>
-                  <td>Created By</td>
-                  <td>Responses</td>
-                  <td>Launch Date</td>
-                  <td>Close Date</td>
-                  <td>Status</td>
-                </tr>
-              </thead>
-              <tbody>
-                {surveyData.slice(startAt, endAt).map((survey) => {
-                  return (
-                    <tr key={survey.id}>
-                      <td>{survey.name}</td>
-                      <td>{survey.created_by}</td>
-                      <td>{survey.response}</td>
-                      <td>{survey.launch_date}</td>
-                      <td>{survey.close_date}</td>
-                      <td>
-                        {survey.status === "Live" ? (
-                          <p style={{ color: "green" }}>{survey.status}</p>
-                        ) : (
-                          <p style={{ color: "red" }}>{survey.status}</p>
-                        )}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-              <div>
-                Rows per page 10 | {pageCount} of{" "}
-                {Math.ceil(surveyData.length / 10)}{" "}
-                <span onClick={backCount}>{"< "}</span>
-                <span onClick={forwardCount}>{" >"}</span>
-              </div>
-            </table>
-          </div>
-        </section>
+          </table>
+        </div>
+      </section>
 
-        <div>
-          {/* <div
+      <div>
+        {/* <div
             style={{
               display: "flex",
               width: "15%",
@@ -165,9 +164,8 @@ function App() {
             {"  "}
             SmartWage
           </div> */}
-        </div>
       </div>
-    </>
+    </div>
   );
 }
 
